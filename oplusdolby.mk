2ebf569acc5fef7eee3eea9ec399729a711f8e29
Ashwin Devi Srinivasan <astroashwin@outlook.com>
2026-02-19 13:52:34 +0400

    fixup

--- oplusdolby.mk
+++ oplusdolby.mk
@@ -8,9 +8,16 @@ $(call inherit-product, vendor/oneplus/dolby/dolby-vendor.mk)
 
 DOLBY_PATH := vendor/oneplus/dolby
 
+DOLBY_USE_C2 ?= false
+
 # Configs
 PRODUCT_COPY_FILES += \
+    $(DOLBY_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+
+ifeq ($(DOLBY_USE_C2),true)
+PRODUCT_COPY_FILES += \
     $(DOLBY_PATH)/configs/media/media_codecs_c2_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_dolby_audio.xml
+endif
 
 ifneq ($(TARGET_USES_CUSTOM_DOLBY_XML),true)
 PRODUCT_COPY_FILES += \
@@ -39,4 +46,6 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/configs/hidl/dolby_framework_matrix.xml
+ifeq ($(DOLBY_USE_C2),true)
 ODM_MANIFEST_FILES += $(DOLBY_PATH)/configs/hidl/c2_manifest.xml
+endif
